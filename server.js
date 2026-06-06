9034e6c961d61cc0d184cad0a8d47c141e67fde3
Nagaraj <nagarajshenoy20@gmail.com>
2026-06-07 03:44:19 +0530

    Added

--- server.js
+++ server.js
@@ -4,24 +4,19 @@ const authRoutes = require("./routes/auth");
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 
-// MongoDB Connection
 mongoose
   .connect("mongodb://127.0.0.1:27017/placementportal")
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.log("❌ MongoDB Error:", err));
 
-// Routes
 app.use("/api", authRoutes);
 
-// Home Route
 app.get("/", (req, res) => {
   res.send("Placement Portal Backend Running");
 });
 
-// Server Start
 app.listen(5000, () => {
   console.log("🚀 Server running on port 5000");
 });
